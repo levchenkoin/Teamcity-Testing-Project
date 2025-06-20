e790a51a8d0dff85d2e38ffea280bfb718527560
Irina Levchenko <irina.levchenko@semrush.com>
2025-06-20 17:41:40 +0200

    buildTypes element selector fix 2.0

--- src/main/java/com/example/teamcity/ui/pages/project/MainPage.java
+++ src/main/java/com/example/teamcity/ui/pages/project/MainPage.java
@@ -3,17 +3,26 @@ package com.example.teamcity.ui.pages.project;
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.Selenide;
-import com.example.teamcity.ui.Selectors;
 import com.example.teamcity.ui.elements.BuildTypeElement;
 import com.example.teamcity.ui.pages.Page;
 
 import java.util.List;
 
+import org.openqa.selenium.By;
+
 import static com.codeborne.selenide.Selenide.elements;
 
 public class MainPage extends Page {
     private static final String PROJECT_URL = "/project/";
-    private ElementsCollection buildTypes = elements(Selectors.byClassStartsWith("BuildsByBuildType__list--"));
+    private ElementsCollection buildTypes = elements(
+    By.cssSelector("[class^='BuildsByBuildType__item'], [class^='BuildTypes__item']")
+);
+
+ /*    private ElementsCollection buildTypes = elements(Selectors.or(
+        Selectors.byClassStartsWith("BuildsByBuildType__item"),
+        Selectors.byClassStartsWith("BuildTypes__item")
+    )
+);*/
     //private final ElementsCollection buildTypes = elements(Selectors.byClass("BuildTypes__item--UX"));
 
     public MainPage open(String projectName) {
